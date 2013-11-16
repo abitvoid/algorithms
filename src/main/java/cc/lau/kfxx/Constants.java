8dc1d04a88765670d1a2ab8eced1ce45391e8c04
lau <leyougor@gmail.com>
2013-11-16 18:32:44 +0800

    delete unused codes

--- src/main/java/cc/lau/kfxx/Constants.java
+++ src/main/java/cc/lau/kfxx/Constants.java
@@ -1,3 +1,4 @@
+
 package cc.lau.kfxx;
 
 public interface Constants {
@@ -5,11 +6,14 @@ public interface Constants {
     public final int DEFAULT_INSERT_LIMIT = 5000;
     public final int THREAD_COUNT = 4;
 
+    public final String DATA_HOME = "/home/lau/bigdata";
+//    public final String DATA_HOME = "/media/lau/Data/bigdata";
+
     public interface Linux {
-        public String fname = "/media/lau/Data/bigdata/2000W/1-200W.csv";
-        public String last5k = "/media/lau/Data/bigdata/2000W/last5000.csv";
-        public String partDir = "/media/lau/Data/bigdata/part";
-        public String totalDir = "/media/lau/Data/bigdata/2000W";
+        public String fname = DATA_HOME + "/2000W/1-200W.csv";
+        public String last5k = DATA_HOME + "/2000W/last5000.csv";
+        public String partDir = DATA_HOME + "/part";
+        public String totalDir = DATA_HOME + "/2000W";
     }
 
 }
